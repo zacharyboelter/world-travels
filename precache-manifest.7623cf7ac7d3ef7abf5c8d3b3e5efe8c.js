889d91cebc230557c042fa173a4dd6e411ffb660
Zachary Boelter <zboelter@gmail.com>
2022-05-18 14:52:54 -0500

    Updates

---
+++ precache-manifest.7623cf7ac7d3ef7abf5c8d3b3e5efe8c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fce38468984c04d39ec8",
+    "url": "/world-travels/static/css/main.caf0b13e.chunk.css"
+  },
+  {
+    "revision": "fce38468984c04d39ec8",
+    "url": "/world-travels/static/js/main.fce38468.chunk.js"
+  },
+  {
+    "revision": "d81810a4e64de93cb072",
+    "url": "/world-travels/static/js/1.d81810a4.chunk.js"
+  },
+  {
+    "revision": "cf36c9a735b886578a69",
+    "url": "/world-travels/static/js/runtime~main.cf36c9a7.js"
+  },
+  {
+    "revision": "bb5b75c073b2f8e97a4727568b59b61a",
+    "url": "/world-travels/index.html"
+  }
+];
