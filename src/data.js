22fd54ea7e290fbd92e09d098e57de06ef1dc892
Zachary Boelter <zboelter@gmail.com>
2022-05-18 13:10:02 -0500

    data seeded in data.js file

---
+++ src/data.js
@@ -0,0 +1,47 @@
+export default [
+    {
+        title: "Chicago",
+        location: "USA",
+        googleMapsUrl: "https://goo.gl/maps/7o1sgZCbwHbM9NhT7",
+        startDate: "26 Apr, 2021",
+        endDate: "28 May, 2022",
+        description: "Chicago has a very well-known culture. Some of the many things Chicago is famous for are: Chicago-style hot dogs, Chicago-style (deep dish) pizza, Maxwell Street Polish Sausage, jazz music, and 1920s gangsters, for example Al Capone. Chicago is also known for architecture, for example the Sears Tower and museums.",
+        imageUrl: "chicago.png"
+    },
+    {
+        title: "Melbourne",
+        location: "Australia",
+        googleMapsUrl: "https://goo.gl/maps/hqo7i8KmaibrHZeZA",
+        startDate: "13 Sep, 2016",
+        endDate: "24 Jan, 2019",
+        description: "Melbourne is known for being one of the most liveable cities on earth. Often referred to as 'the Sporting Capital of the World', besides this it is also famous for its graffitied laneways, excellent coffee, cultural diversity and bayside location. This eclectic Australian city has something for everyone.",
+        imageUrl: "luna-park.png"
+    },
+    {
+        title: "Merida",
+        location: "Mexico",
+        googleMapsUrl: "https://goo.gl/maps/LJbDuChtkRD8iH7S7",
+        startDate: "14 Apr, 2014",
+        endDate: "4 May, 2019",
+        description: "Merida is the largest city in Mexico's wild and wonderful Yucatan Peninsula. With an interesting mix of Mayan and colonial culture, the city is encircled by ancient temples, crumbling ruins, sacred cenotes, and colorful mansions.",
+        imageUrl: "yucatan.png"
+    },
+    {
+        title: "Tokyo",
+        location: "Japan",
+        googleMapsUrl: "https://goo.gl/maps/t1PGNw39mGk2iQn29",
+        startDate: "13 May, 2019",
+        endDate: "24 Aug, 2019",
+        description: "Tokyo, the world's largest metropolis, is known for top-rated restaurants, the Shibuya Crossing, Imperial Palace, cherry blossoms, markets, and more. The shopping scene includes the Harajuku and Akihabara districts. Tokyo is also famous for its vending machines, cat cafes, museums, and sacred sites.",
+        imageUrl: "tokyo.png"
+    },
+    {
+        title: "Amsterdam",
+        location: "Netherlands",
+        googleMapsUrl: "https://goo.gl/maps/SauXDxchYvhV6wuU8",
+        startDate: "13 Aug, 2023",
+        endDate: "24 Oct, 2023",
+        description: "Amsterdam is famous for its fine culture, rich history, beautiful canals, coffeeshops, and its famous Red Light District. Because of this, Amsterdam was visited by 9 million tourists in 2019. If you are looking for a city that pulls you in with its art, charm, grace, and beauty, then look no further than Amsterdam.",
+        imageUrl: "amsterdam.png"
+    },
+]
