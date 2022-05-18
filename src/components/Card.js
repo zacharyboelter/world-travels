dff01508fb30c0b85146d2495de77497f8061d8b
Zachary Boelter <zboelter@gmail.com>
2022-05-18 14:44:29 -0500

    styling

--- src/components/Card.js
+++ src/components/Card.js
@@ -10,8 +10,8 @@ export default function Card(props) {
                     <h6 className="country">{props.location}</h6>
                     <a href={props.googleMapsUrl} className="google--url">View on Google Maps</a>
                 </div>
-                <h2>{props.title}</h2>
-                <h4>{props.startDate} - {props.endDate}</h4>
+                <h2 className="city">{props.title}</h2>
+                <h4 className="dates">{props.startDate} - {props.endDate}</h4>
                 <p className="description">{props.description}</p>
 
             </div>
