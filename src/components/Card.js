6815a90ddb57191b33527c3498857490ab59e375
Zachary Boelter <zboelter@gmail.com>
2022-05-18 13:57:33 -0500

    props loaded and only need styling done?

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,19 @@
 import React from "react";
 
-export default function Card(props){
+export default function Card(props) {
     return (
         <div className="card">
-            <img src={`../images/${props.imageUrl}`} alt="" className="card--img"/>
+            <img src={`../images/${props.imageUrl}`} alt="" className="card--img" />
             <div className="card-info">
-                <img src="../images/pin-icon.png" alt="" className="pin--icon"/>
-                
+                <div className="card--top">
+                    <img src="../images/pin-icon.png" alt="" className="pin--icon" />
+                    <h6 className="country">{props.location}</h6>
+                    <a href={props.googleMapsUrl} className="google--url">View on Google Maps</a>
+                </div>
+                <h2>{props.title}</h2>
+                <h4>{props.startDate} - {props.endDate}</h4>
+                <p className="description">{props.description}</p>
+
             </div>
         </div>
     )
