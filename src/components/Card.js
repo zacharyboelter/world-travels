74024f3b8c51f625e6c57294c5590b4f4a093cd3
Zachary Boelter <zboelter@gmail.com>
2022-05-18 15:01:10 -0500

    links fixed

--- src/components/Card.js
+++ src/components/Card.js
@@ -8,7 +8,7 @@ export default function Card(props) {
                 <div className="card--top">
                     <img src="../images/pin-icon.png" alt="" className="pin--icon" />
                     <h6 className="country">{props.location}</h6>
-                    <a href={props.googleMapsUrl} className="google--url">View on Google Maps</a>
+                    <a href={props.googleMapsUrl} target="_blank" className="google--url">View on Google Maps</a>
                 </div>
                 <h2 className="city">{props.title}</h2>
                 <h4 className="dates">{props.startDate} - {props.endDate}</h4>
